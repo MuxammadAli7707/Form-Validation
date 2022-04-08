31ad96d71c46ad45aaacc9cc296ee10627d7ab23
MuxammadAli7707 <muxammadali7707@gmail.com>
2022-04-08 12:06:26 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const elForm = document.getElementById("form");
+const elName = document.getElementById("fname");
+const elMail = document.getElementById("fmail");
+const elNumber = document.getElementById("fnumber");
+const elPassword = document.getElementById("fpassword");
+const elPasswords = document.getElementById("fpasswords");
+const elBtn = document.getElementById("fbtn");
+
+elForm.addEventListener("submit", (e) =>{
+  e.preventDefault();
+});
